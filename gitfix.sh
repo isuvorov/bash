2fab4aa0cc04d047306b64454ea1d4b5ee408190
Igor Suvorov <hi@isuvorov.com>
2020-06-17 13:56:14 +0300

    fix: add gitfix alias

---
+++ gitfix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LOGS=${@}
+
+cmd="git add. && git commit -m \"fix: $LOGS\" && git push"
+
+echo "$cmd"
+
+git add .
+git commit -m "fix: $LOGS"
+git push
+# ssh primary.buzz.guru $cmd | bunyan -o short ${@:3}
+# ssh primary.buzz.guru $cmd | ~/projects/lego-starter-kit/bunyan
