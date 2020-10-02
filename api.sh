aa4879a0d0fccbc80e2b306ec837f018e1c71bfa
Igor Suvorov <hi@isuvorov.com>
2020-10-02 09:15:16 +0300

    feat: add 2 second repeat while error

--- api.sh
+++ api.sh
@@ -4,8 +4,17 @@ ARGS=${@}
 
 PORT=`node -e "console.log(require('./.env.js').port || 8080)"`
 
-cmd="http http://localhost:$PORT$ARGS"
+cmd="http --check-status http://localhost:$PORT$ARGS"
 
 echo "$cmd"
 
 $cmd
+
+while [ $? -ne 0 ]; do
+  echo "ERROR, repeat in 2 second"
+  sleep 1
+  echo ""
+  sleep 1
+  echo "$cmd"
+  $cmd
+done
