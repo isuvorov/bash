8e1d31b1b613f8f7541dbe9b7bf4a1a3ffd53a64
Igor Suvorov <hi@isuvorov.com>
2021-09-23 20:42:33 +0300

    fix: pds logs

--- bash/utils/pdslogs.sh
+++ bash/utils/pdslogs.sh
@@ -15,8 +15,8 @@ LOGS=${2-10}
 
 cmd="docker service logs $1 -f --tail $LOGS --raw"
 
-echo "ssh ${SERVER} \"${SERVER_PREFIX}$cmd\" | lsklog -o short ${@:3}"
+echo "ssh ${SERVER} \"${SERVER_PREFIX}$cmd\" | lsk log -o short ${@:3}"
 # ssh ${SERVER} ${SERVER_PREFIX}$cmd | ~/bash/bunyan.js -o short ${@:3}
-ssh ${SERVER} ${SERVER_PREFIX}$cmd | /Users/isuvorov/projects/lskjs/packages/log2/build/bin/lsklog.js -o short ${@:3}
+ssh ${SERVER} ${SERVER_PREFIX}$cmd | lsk log -o short ${@:3}
 # -l trace
 # ssh primary.buzz.guru $cmd | ~/projects/lego-starter-kit/bunyan
